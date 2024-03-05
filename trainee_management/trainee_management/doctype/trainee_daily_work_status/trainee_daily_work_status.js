4daae524c7faf9bf239b8677791716a6ed937acd
tusharpatil3688 <tusharpatil3688@gmail.com>
2024-03-05 12:20:41 +0530

    intern-nddb : Update date validation in daily work status

--- trainee_management/trainee_management/doctype/trainee_daily_work_status/trainee_daily_work_status.js
+++ trainee_management/trainee_management/doctype/trainee_daily_work_status/trainee_daily_work_status.js
@@ -2,9 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Trainee Daily Work Status', {
-	refresh(frm) {
-		frm.set_value("date", frappe.datetime.get_today());
+	// refresh(frm) {
+	// 	frm.set_value("date", frappe.datetime.get_today());
 
-	},
+	// },
 	
 		});
